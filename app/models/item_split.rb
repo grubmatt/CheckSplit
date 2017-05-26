bc05ed79aa6742c6c5502ae7317ccd09f98141ff
Matt <mattgruber61@gmail.com>
2017-05-25 22:02:32 -0400

    Setup associations.

--- app/models/item_split.rb
+++ app/models/item_split.rb
@@ -1,2 +1,5 @@
 class ItemSplit < ActiveRecord::Base
+	belongs_to :person
+	belongs_to :order_item
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,8 @@
 class Person < ActiveRecord::Base
+	has_secure_password
+
+	has_many :orders
+	has_many :item_splits
+
+	
 end
